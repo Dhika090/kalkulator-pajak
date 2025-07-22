9720932682288433ca945312864b855f5fd50a3c
dhika090 <nyes6953@gmail.com>
2025-07-22 22:19:36 +0700

    fix pph4(2)&pph23

--- src/pages/pph23.js
+++ src/pages/pph23.js
@@ -2,96 +2,229 @@ import { useState } from "react";
 import Select from "react-select";
 
 const kodeObjekPajakList = [
+  { kode: "24-101-01", deskripsi: "Dividen", tarif: 15 },
+  { kode: "24-104-05", deskripsi: "Jasa Aktuaris", tarif: 2 },
+  { kode: "24-104-08", deskripsi: "Jasa Arsitektur", tarif: 2 },
   {
-    kode: "24-100-01",
-    deskripsi: "Hadiah, Penghargaan, Bonus dan Lainnya",
-    tarif: 15,
-  },
-  {
-    kode: "24-100-02",
-    deskripsi: "Sewa dan penghasilan lain sehubungan dengan penggunaan Harta",
+    kode: "24-104-13",
+    deskripsi:
+      "Jasa Penambangan dan Jasa Penunjang di Bidang Usaha Panas Bumi dan Penambangan Minyak dan Gas Bumi (Migas)",
     tarif: 2,
   },
-  { kode: "24-101-01", deskripsi: "Dividen", tarif: 15 },
   {
     kode: "24-102-01",
-    deskripsi: "Bunga Selain yang dikenali PPh Pasal 4 ayat (2)",
+    deskripsi: "Bunga Selain yang Dikenakan PPh Pasal 4 ayat (2)",
     tarif: 15,
   },
+  { kode: "24-104-15", deskripsi: "Jasa Penebangan Hutan", tarif: 2 },
+  { kode: "24-104-16", deskripsi: "Jasa Pengolahan Limbah", tarif: 2 },
+  {
+    kode: "24-104-17",
+    deskripsi:
+      "Jasa Penyedia Tenaga Kerja dan/atau Tenaga Ahli (Outsourcing Services)",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-18",
+    deskripsi: "Jasa Perantara dan/atau Keagenan",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-19",
+    deskripsi:
+      "Jasa Bidang Perdagangan Surat-Surat Berharga, Kecuali yang Dilakukan Bursa Efek, Kustodian Sentral Efek Indonesia (KSEI) dan Kliring Penjaminan Efek Indonesia (KPEI)",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-20",
+    deskripsi:
+      "Jasa Kustodian/Penyimpanan/Penitipan, Kecuali yang Dilakukan Oleh KSEI",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-21",
+    deskripsi: "Jasa Pengisian Suara (Dubbing) dan/atau Sulih Suara",
+    tarif: 2,
+  },
+  { kode: "24-104-22", deskripsi: "Jasa Mixing Film", tarif: 2 },
+  {
+    kode: "24-104-23",
+    deskripsi:
+      "Jasa Pembuatan Sarana Promosi Film, Iklan, Poster, Foto, Slide, Klise, Banner, Pamphlet, Baliho dan Folder",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-24",
+    deskripsi:
+      "Jasa Sehubungan Dengan Software Atau Hardware Atau Sistem Komputer, Termasuk Perawatan, Pemeliharaan dan Perbaikan.",
+    tarif: 2,
+  },
   { kode: "24-103-01", deskripsi: "Royalti", tarif: 15 },
+  {
+    kode: "24-104-25",
+    deskripsi: "Jasa Pembuatan dan/atau Pengelolaan Website",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-26",
+    deskripsi: "Jasa Internet Termasuk Sambungannya",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-27",
+    deskripsi:
+      "Jasa Penyimpanan, Pengolahan dan/atau Penyaluran Data, Informasi, dan/atau Program",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-28",
+    deskripsi:
+      "Jasa Instalasi/Pemasangan Mesin, Peralatan, Listrik, Telepon, Air, Gas, Ac dan/atau Tv Kabel, Selain Yang Dilakukan Oleh Wajib Pajak Yang Ruang Lingkupnya Di Bidang Konstruksi dan Mempunyai Izin dan/atau Sertifikasi Sebagai Pengusaha Konstruksi;",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-29",
+    deskripsi:
+      "Jasa Perawatan/Perbaikan/Pemeliharaan Mesin, Peralatan, Listrik, Telepon, Air, Gas, Ac dan/atau Tv Kabel, Selain Yang Dilakukan Oleh Wajib Pajak Yang Ruang Lingkupnya di Bidang Konstruksi dan Mempunyai Izin dan/atau Sertifikasi Sebagai Pengusaha Konstruksi",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-30",
+    deskripsi:
+      "Jasa Perawatan Kendaraan dan/atau Alat Transportasi Darat, Laut dan Udara",
+    tarif: 2,
+  },
+  { kode: "24-104-31", deskripsi: "Jasa Maklon", tarif: 2 },
+  { kode: "24-104-32", deskripsi: "Jasa Penyelidikan dan Keamanan", tarif: 2 },
+  {
+    kode: "24-104-33",
+    deskripsi: "Jasa Penyelenggara Kegiatan Atau Event Organizer",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-34",
+    deskripsi:
+      "Jasa Penyediaan Tempat dan/atau Waktu Dalam Media Massa, Media Luar Ruang Atau Media Lain Untuk Penyampaian Informasi, dan/atau Jasa Periklanan",
+    tarif: 2,
+  },
+  {
+    kode: "24-100-01",
+    deskripsi:
+      "Hadiah, Penghargaan, Bonus dan Lainnya Selain yang Telah Dipotong PPh Pasal 21 Ayat (1) Huruf E UU PPh",
+    tarif: 15,
+  },
+  { kode: "24-104-35", deskripsi: "Jasa Pembasmian Hama", tarif: 2 },
+  {
+    kode: "24-104-36",
+    deskripsi: "Jasa Kebersihan Atau Cleaning Service",
+    tarif: 2,
+  },
+  { kode: "24-104-37", deskripsi: "Jasa Sedot Septic Tank", tarif: 2 },
+  { kode: "24-104-38", deskripsi: "Jasa Pemeliharaan Kolam", tarif: 2 },
+  { kode: "24-104-39", deskripsi: "Jasa Katering Atau Tata Boga", tarif: 2 },
+  { kode: "24-104-40", deskripsi: "Jasa Freight Forwarding", tarif: 2 },
+  { kode: "24-104-41", deskripsi: "Jasa Logistik", tarif: 2 },
+  { kode: "24-104-42", deskripsi: "Jasa Pengurusan Dokumen", tarif: 2 },
+  { kode: "24-104-43", deskripsi: "Jasa Pengepakan", tarif: 2 },
+  { kode: "24-104-44", deskripsi: "Jasa Loading dan Unloading", tarif: 2 },
+  {
+    kode: "24-100-02",
+    deskripsi:
+      "Sewa dan Penghasilan Lain Sehubungan Dengan Penggunaan Harta Kecuali Sewa Tanah dan/atau Bangunan yang Telah Dikenai PPh Pasal 4 Ayat (2) UU PPh.",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-45",
+    deskripsi:
+      "Jasa Laboratorium dan/atau Pengujian Kecuali yang Dilakukan oleh Lembaga atau Institusi Pendidikan Dalam Rangka Penelitian Akademis",
+    tarif: 2,
+  },
+  { kode: "24-104-46", deskripsi: "Jasa Pengelolaan Parkir", tarif: 2 },
+  { kode: "24-104-47", deskripsi: "Jasa Penyondiran Tanah", tarif: 2 },
+  {
+    kode: "24-104-48",
+    deskripsi: "Jasa Penyiapan dan/atau Pengolahan Lahan",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-49",
+    deskripsi: "Jasa Pembibitan dan/atau Penanaman Bibit",
+    tarif: 2,
+  },
+  { kode: "24-104-50", deskripsi: "Jasa Pemeliharaan Tanaman", tarif: 2 },
+  {
+    kode: "24-104-51",
+    deskripsi: "Jasa Pemanenan",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-52",
+    deskripsi:
+      "Jasa Pengolahan Hasil Pertanian, Perkebunan, Perikanan, Peternakan dan/atau Perhutanan",
+    tarif: 2,
+  },
+  { kode: "24-104-53", deskripsi: "Jasa Dekorasi", tarif: 2 },
+  { kode: "24-104-54", deskripsi: "Jasa Pencetakan/Penerbitan", tarif: 2 },
   { kode: "24-104-01", deskripsi: "Jasa Teknik", tarif: 2 },
+  { kode: "24-104-55", deskripsi: "Jasa Penerjemahan", tarif: 2 },
+  {
+    kode: "24-104-56",
+    deskripsi:
+      "Jasa Pengangkutan/Ekspedisi Kecuali Yang Telah Diatur Dalam Pasal 15 Undang-Undang Pajak Penghasilan",
+    tarif: 2,
+  },
+  { kode: "24-104-57", deskripsi: "Jasa Pelayanan Pelabuhan", tarif: 2 },
+  {
+    kode: "24-104-58",
+    deskripsi: "Jasa Pengangkutan Melalui Jalur Pipa",
+    tarif: 2,
+  },
+  { kode: "24-104-59", deskripsi: "Jasa Pengelolaan Penitipan Anak", tarif: 2 },
+  { kode: "24-104-60", deskripsi: "Jasa Pelatihan dan/atau Kursus", tarif: 2 },
+  {
+    kode: "24-104-61",
+    deskripsi: "Jasa Pengiriman dan Pengisian Uang Ke Atm",
+    tarif: 2,
+  },
+  { kode: "24-104-62", deskripsi: "Jasa Sertifikasi", tarif: 2 },
+  { kode: "24-104-63", deskripsi: "Jasa Survey", tarif: 2 },
+  { kode: "24-104-64", deskripsi: "Jasa Tester", tarif: 2 },
   { kode: "24-104-02", deskripsi: "Jasa Manajemen", tarif: 2 },
+  {
+    kode: "24-104-65",
+    deskripsi:
+      "Jasa Selain Jasa-Jasa Tersebut di Atas yang Pembayarannya Dibebankan pada APBN (Anggaran Pendapatan dan Belanja Negara) Atau APBD (Anggaran Pendapatan dan Belanja Daerah).",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-66",
+    deskripsi:
+      "Jasa Penyelenggaraan Layanan Transaksi Pembayaran Terkait dengan Distribusi Token Oleh Penyelenggara Distribusi",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-67",
+    deskripsi: "Jasa Pemasaran dengan Media Voucer Oleh Penyelenggara Voucer",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-68",
+    deskripsi:
+      "Jasa Penyelenggaraan Layanan Transaksi Pembayaran Terkait dengan Distribusi Voucer Oleh Penyelenggara Voucer dan Penyelenggara  Distribusi",
+    tarif: 2,
+  },
+  {
+    kode: "24-104-69",
+    deskripsi:
+      "Jasa Penyelenggaraan Program Loyalitas dan Penghargaan Pelanggan (Consumer Loyalty/Reward Program)  Oleh Penyelenggara Voucer",
+    tarif: 2,
+  },
   { kode: "24-104-03", deskripsi: "Jasa Konsultan", tarif: 2 },
-  { kode: "24-104-04", deskripsi: "Jasa Penilai (Appraisal)", tarif: 2 },
-  { kode: "24-104-05", deskripsi: "Jasa Aktuaris", tarif: 2 },
-  { kode: "24-104-06", deskripsi: "Jasa Akuntansi", tarif: 2 },
-  { kode: "24-104-07", deskripsi: "Jasa Hukum", tarif: 2 },
-  { kode: "24-104-08", deskripsi: "Jasa Notaris", tarif: 2 },
-  { kode: "24-104-09", deskripsi: "Jasa Perancang", tarif: 2 },
-  { kode: "24-104-10", deskripsi: "Jasa Arsitektur", tarif: 2 },
-  { kode: "24-104-11", deskripsi: "Jasa Perancang Perangkat Lunak", tarif: 2 },
-  { kode: "24-104-12", deskripsi: "Jasa Desain", tarif: 2 },
-  { kode: "24-104-13", deskripsi: "Jasa Teknik Sipil", tarif: 2 },
-  { kode: "24-104-14", deskripsi: "Jasa Perawatan/Pemeliharaan", tarif: 2 },
-  { kode: "24-104-15", deskripsi: "Jasa Kebersihan", tarif: 2 },
-  { kode: "24-104-16", deskripsi: "Jasa Catering", tarif: 2 },
-  { kode: "24-104-17", deskripsi: "Jasa Periklanan", tarif: 2 },
-  { kode: "24-104-18", deskripsi: "Jasa Penelitian & Pengembangan", tarif: 2 },
-  { kode: "24-104-19", deskripsi: "Jasa Pengelolaan Parkir", tarif: 2 },
-  { kode: "24-104-20", deskripsi: "Jasa Penyedia Tenaga Kerja", tarif: 2 },
-  { kode: "24-104-21", deskripsi: "Jasa Pelatihan", tarif: 2 },
-  { kode: "24-104-22", deskripsi: "Jasa Penerjemahan", tarif: 2 },
-  { kode: "24-104-23", deskripsi: "Jasa Fotografi", tarif: 2 },
-  { kode: "24-104-24", deskripsi: "Jasa Videografi", tarif: 2 },
-  { kode: "24-104-25", deskripsi: "Jasa Pengurusan Transportasi", tarif: 2 },
-  { kode: "24-104-26", deskripsi: "Jasa Pemeliharaan Software", tarif: 2 },
-  { kode: "24-104-27", deskripsi: "Jasa Pembuatan Perangkat Lunak", tarif: 2 },
-  { kode: "24-104-28", deskripsi: "Jasa Keamanan", tarif: 2 },
-  { kode: "24-104-29", deskripsi: "Jasa Penyewaan Peralatan", tarif: 2 },
-  { kode: "24-104-30", deskripsi: "Jasa Pengangkutan Barang", tarif: 2 },
-  { kode: "24-104-31", deskripsi: "Jasa Instalasi Mesin/Peralatan", tarif: 2 },
-  { kode: "24-104-32", deskripsi: "Jasa Pengurusan Dokumen", tarif: 2 },
-  { kode: "24-104-33", deskripsi: "Jasa Konstruksi", tarif: 2 },
-  { kode: "24-104-34", deskripsi: "Jasa Pertambangan", tarif: 2 },
-  { kode: "24-104-35", deskripsi: "Jasa Pengelolaan Limbah", tarif: 2 },
-  { kode: "24-104-36", deskripsi: "Jasa Pembersihan Tangki", tarif: 2 },
-  { kode: "24-104-37", deskripsi: "Jasa Inspeksi Teknik", tarif: 2 },
-  { kode: "24-104-38", deskripsi: "Jasa Pelayanan Medis", tarif: 2 },
-  { kode: "24-104-39", deskripsi: "Jasa Survey", tarif: 2 },
-  { kode: "24-104-40", deskripsi: "Jasa Penyelidikan", tarif: 2 },
-  { kode: "24-104-41", deskripsi: "Jasa Pengelolaan Informasi", tarif: 2 },
-  { kode: "24-104-42", deskripsi: "Jasa Asistensi Medis", tarif: 2 },
-  { kode: "24-104-43", deskripsi: "Jasa Pengelolaan Air", tarif: 2 },
-  { kode: "24-104-44", deskripsi: "Jasa Laboratorium", tarif: 2 },
-  { kode: "24-104-45", deskripsi: "Jasa Konsultasi Hukum", tarif: 2 },
-  { kode: "24-104-46", deskripsi: "Jasa Sertifikasi", tarif: 2 },
-  { kode: "24-104-47", deskripsi: "Jasa Pendidikan", tarif: 2 },
-  { kode: "24-104-48", deskripsi: "Jasa Telekomunikasi", tarif: 2 },
-  { kode: "24-104-49", deskripsi: "Jasa Event Organizer", tarif: 2 },
-  { kode: "24-104-50", deskripsi: "Jasa Lelang", tarif: 2 },
-  { kode: "24-104-51", deskripsi: "Jasa Logistik", tarif: 2 },
-  { kode: "24-104-52", deskripsi: "Jasa Pemrosesan Data", tarif: 2 },
-  { kode: "24-104-53", deskripsi: "Jasa Pembelian", tarif: 2 },
-  { kode: "24-104-54", deskripsi: "Jasa Evaluasi", tarif: 2 },
-  { kode: "24-104-55", deskripsi: "Jasa Audit", tarif: 2 },
-  { kode: "24-104-56", deskripsi: "Jasa Perpajakan", tarif: 2 },
-  { kode: "24-104-57", deskripsi: "Jasa Humas", tarif: 2 },
-  { kode: "24-104-58", deskripsi: "Jasa Penelitian Pasar", tarif: 2 },
-  { kode: "24-104-59", deskripsi: "Jasa Pengurusan Visa", tarif: 2 },
-  { kode: "24-104-60", deskripsi: "Jasa Pembuatan Video", tarif: 2 },
-  { kode: "24-104-61", deskripsi: "Jasa Pengelolaan Gudang", tarif: 2 },
-  { kode: "24-104-62", deskripsi: "Jasa Reparasi", tarif: 2 },
-  { kode: "24-104-63", deskripsi: "Jasa Pengolahan Data", tarif: 2 },
-  { kode: "24-104-64", deskripsi: "Jasa Distribusi", tarif: 2 },
-  { kode: "24-104-65", deskripsi: "Jasa Penyusunan Laporan", tarif: 2 },
-  { kode: "24-104-66", deskripsi: "Jasa Penerbitan", tarif: 2 },
-  { kode: "24-104-67", deskripsi: "Jasa Arkeologi", tarif: 2 },
-  { kode: "24-104-68", deskripsi: "Jasa Pengelolaan Arsip", tarif: 2 },
-  { kode: "24-104-69", deskripsi: "Jasa Penyewaan Software", tarif: 2 },
 ];
 
 export default function PPh23Calculator() {
-  const [selectedKode, setSelectedKode] = useState("");
+  // const [selectedKode, setSelectedKode] = useState("");
+  const [selectedKode, setSelectedKode] = useState(null);
   const [penghasilanBruto, setPenghasilanBruto] = useState("");
   const [tarif, setTarif] = useState(0);
   const [pph23, setPph23] = useState(0);
@@ -105,14 +238,18 @@ export default function PPh23Calculator() {
     }).format(value);
 
   // Handle perubahan dropdown
-  const handleKodeChange = (event) => {
-    const kode = event.target.value;
-    setSelectedKode(kode);
-    const selectedItem = kodeObjekPajakList.find((item) => item.kode === kode);
-    if (selectedItem) {
-      setTarif(selectedItem.tarif);
-      calculatePPh23(penghasilanBruto, selectedItem.tarif);
+  const handleKodeChange = (selectedOption) => {
+    if (selectedOption) {
+      const selectedItem = kodeObjekPajakList.find(
+        (item) => item.kode === selectedOption.value
+      );
+      setSelectedKode(selectedOption); // simpan object selectedOption
+      if (selectedItem) {
+        setTarif(selectedItem.tarif);
+        calculatePPh23(penghasilanBruto, selectedItem.tarif);
+      }
     } else {
+      setSelectedKode(null);
       setTarif(0);
       setPph23(0);
     }
@@ -120,7 +257,7 @@ export default function PPh23Calculator() {
 
   // Handle input penghasilan bruto
   const handlePenghasilanChange = (event) => {
-    const value = event.target.value.replace(/\D/g, ""); // Hanya angka
+    const value = event.target.value.replace(/\D/g, "");
     setPenghasilanBruto(value);
     calculatePPh23(value, tarif);
   };
@@ -139,7 +276,7 @@ export default function PPh23Calculator() {
         backgroundImage: "url('/REV1.jpg')",
       }}
     >
-      <div className="bg-white shadow-lg rounded-lg mt-4 p-6 w-full max-w-2xl">
+      <div className="bg-white/80 backdrop-blur-md shadow-2xl rounded-xl p-6 w-full max-w-2xl mt-4 mb-20 sm:mb-40">
         <h2 className="text-2xl font-bold text-gray-700 mb-4">PPh 23</h2>
 
         {/* Dropdown Kode Objek Pajak */}
--- src/pages/pph4(2).js
+++ src/pages/pph4(2).js
@@ -3,162 +3,116 @@ import Select from "react-select";
 
 const taxOptions = [
   {
-    value: "28-401-01",
-    label: "28-401-01 - Bunga Obligasi WP Dalam Negeri",
-    rate: 15,
+    value: "28-409-25",
+    label:
+      "28-409-25 - Pekerjaan Konstruksi Terintegrasi yang Dilakukan oleh Penyedia Jasa yang Memiliki Sertifikat Badan Usaha",
+    rate: 2.65,
   },
   {
-    value: "28-401-02",
-    label: "28-401-02 - Bunga Obligasi WP Luar Negeri",
-    rate: 20,
-  },
-  {
-    value: "28-401-03",
-    label: "28-401-03 - Bunga Obligasi WP Dalam Negeri & BUT",
-    rate: 10,
-  },
-  {
-    value: "28-401-04",
-    label: "28-401-04 - Diskonto Surat Perbendaharaan Negara WP Dalam Negeri",
-    rate: 20,
-  },
-  {
-    value: "28-401-05",
-    label: "28-401-05 - Diskonto Surat Perbendaharaan Negara WP Luar Negeri",
-    rate: 20,
-  },
-  {
-    value: "28-402-01",
-    label: "28-402-01 - Pengalihan Hak atas Tanah/Bangunan",
-    rate: 10,
-  },
-  {
-    value: "28-402-02",
-    label: "28-402-02 - Pengalihan Rumah Sederhana/RS Sederhana",
-    rate: 0,
-  },
-  {
-    value: "28-402-03",
-    label: "28-402-03 - Pengalihan Hak kepada Pemerintah/BUMN",
-    rate: 2.5,
-  },
-  {
-    value: "28-403-01",
-    label: "28-403-01 - Persewaan Tanah/Bangunan",
-    rate: 1,
-  },
-  {
-    value: "28-404-01",
-    label: "28-404-01 - Bunga Tabungan & Diskonto (Non DHE)",
-    rate: 0,
-  },
-  {
-    value: "28-404-02",
-    label: "28-404-02 - Bunga Deposito IDR DHE (1 bulan)",
-    rate: 10,
-  },
-  {
-    value: "28-404-03",
-    label: "28-404-03 - Bunga Deposito IDR DHE (3 bulan)",
-    rate: 10,
-  },
-  {
-    value: "28-404-04",
-    label: "28-404-04 - Bunga Deposito IDR DHE (6 bulan+)",
-    rate: 20,
-  },
-  {
-    value: "28-404-05",
-    label: "28-404-05 - Bunga Deposito USD DHE (1 bulan)",
-    rate: 7.5,
-  },
-  {
-    value: "28-404-06",
-    label: "28-404-06 - Bunga Deposito USD DHE (3 bulan)",
-    rate: 5,
-  },
-  {
-    value: "28-404-07",
-    label: "28-404-07 - Bunga Deposito USD DHE (6 bulan)",
-    rate: 0,
-  },
-  {
-    value: "28-404-08",
-    label: "28-404-08 - Bunga Deposito USD DHE (6 bulan+)",
-    rate: 10,
-  },
-  {
-    value: "28-404-09",
-    label: "28-404-09 - Bunga Deposito di Luar Negeri",
-    rate: 7.5,
-  },
-  { value: "28-404-10", label: "28-404-10 - Diskonto Sertifikat BI", rate: 20 },
-  { value: "28-404-11", label: "28-404-11 - Jasa Giro", rate: 20 },
-  {
-    value: "28-405-01",
-    label: "28-405-01 - Hadiah Undian WP Dalam Negeri",
-    rate: 25,
-  },
-  {
-    value: "28-405-02",
-    label: "28-405-02 - Hadiah Undian WP Luar Negeri",
-    rate: 20,
-  },
-  {
-    value: "28-406-01",
-    label: "28-406-01 - Penjualan Saham Bursa (Non Pendiri)",
-    rate: 0.1,
-  },
-  {
-    value: "28-407-01",
-    label: "28-407-01 - Penjualan Saham Bursa (Pendiri)",
-    rate: 0.5,
-  },
-  {
-    value: "28-408-01",
-    label: "28-408-01 - Penjualan Saham Modal Ventura (Non Bursa)",
-    rate: 5,
-  },
-  {
-    value: "28-409-15",
-    label: "28-409-15 - Jasa Konstruksi Kualifikasi Kecil",
-    rate: 2,
-  },
-  {
-    value: "28-409-16",
-    label: "28-409-16 - Jasa Konstruksi Tanpa Sertifikat",
+    value: "28-409-26",
+    label:
+      "28-409-26 - Pekerjaan Konstruksi Terintegrasi yang Dilakukan oleh Penyedia Jasa yang Tidak Memiliki Sertifikat Badan Usaha",
     rate: 4,
   },
   {
-    value: "28-409-17",
-    label: "28-409-17 - Jasa Konstruksi Kualifikasi Non-Kecil",
+    value: "28-409-27",
+    label:
+      "28-409-27 - Jasa Konsultansi Konstruksi yang Dilakukan oleh Penyedia Jasa yang Memiliki Sertifikat Badan Usaha atau Sertifikat Kompetensi Kerja untuk Usaha Orang Perseorangan",
+    rate: 3.5,
+  },
+  {
+    value: "28-409-28",
+    label:
+      "28-409-28 - Jasa Konsultansi Konstruksi yang Dilakukan oleh Penyedia Jasa yang Tidak Memiliki Sertifikat Badan Usaha atau Sertifikat Kompetensi Kerja untuk Usaha Orang Perseorangan",
+    rate: 6,
+  },
+  {
+    value: "28-409-08",
+    label:
+      "28-409-08 - Jasa Konstruksi Berupa Jasa Perencanaan Konstruksi (Dengan Kualifikasi Usaha)",
+    rate: 4,
+  },
+  {
+    value: "28-409-09",
+    label:
+      "28-409-09 - Jasa Konstruksi Berupa Jasa Perencanaan Konstruksi (Tanpa Kualifikasi Usaha)",
+    rate: 6,
+  },
+  {
+    value: "28-409-10",
+    label:
+      "28-409-10 - Jasa Konstruksi Berupa Jasa Pelaksanaan Konstruksi (Kualifikasi Usaha Kecil)",
+    rate: 2,
+  },
+  {
+    value: "28-409-11",
+    label:
+      "28-409-11 - Jasa Konstruksi Berupa Jasa Pelaksanaan Konstruksi (Kualifikasi Usaha Menengah dan Besar)",
     rate: 3,
   },
   {
-    value: "28-417-01",
-    label: "28-417-01 - Bunga Simpanan Koperasi (≤ Rp240.000)",
-    rate: 0,
+    value: "28-409-12",
+    label:
+      "28-409-12 - Jasa Konstruksi Berupa Jasa Pelaksanaan Konstruksi (Tanpa Kualifikasi Usaha)",
+    rate: 4,
   },
   {
-    value: "28-417-02",
-    label: "28-417-02 - Bunga Simpanan Koperasi (> Rp240.000)",
-    rate: 10,
+    value: "28-409-13",
+    label:
+      "28-409-13 - Jasa Konstruksi Berupa Jasa Pengawasan Konstruksi (Dengan Kualifikasi Usaha)",
+    rate: 4,
+  },
+  {
+    value: "28-409-14",
+    label:
+      "28-409-14 - Jasa Konstruksi Berupa Jasa Pengawasan Konstruksi (Tanpa Kualifikasi Usaha)",
+    rate: 6,
   },
   {
     value: "28-419-01",
-    label: "28-419-01 - Dividen WP Orang Pribadi",
+    label:
+      "28-419-01 - Dividen yang Diterima/Diperoleh Wajib Pajak Orang Pribadi Dalam Negeri",
     rate: 10,
   },
-  { value: "28-421-01", label: "28-421-01 - Uplift Hulu Migas", rate: 20 },
   {
     value: "28-423-01",
-    label: "28-423-01 - Transaksi dengan WP PP 23/2018",
-    rate: 0.5,
+    label:
+      "28-423-01 - Pemotongan atau pemungutan PPh atas penjualan barang atau penyerahan jasa yang dilakukan oleh Wajib Pajak dengan peredaran bruto tertentu sesuai dengan Peraturan Pemerintah Nomor 23 Tahun 2018 atau Peraturan Pemerintah Nomor 55 Tahun 2022.",
+    rate: 5,
   },
   {
-    value: "28-499-02",
-    label: "28-499-02 - Penghasilan dari Kerja Sama dengan LPI",
+    value: "28-423-02",
+    label:
+      "28-423-02 - Pemotongan atau pemungutan PPh atas transaksi pembelian yang dilakukan oleh Wajib Pajak dengan peredaran bruto tertentu sesuai dengan Peraturan Pemerintah Nomor 55 Tahun 2022.",
+    rate: 0,
+  },
+  {
+    value: "28-403-02",
+    label: "28-403-02 - Persewaan Tanah dan/atau Bangunan",
     rate: 10,
+  },
+  {
+    value: "28-405-01",
+    label: "28-405-01 - Hadiah Undian",
+    rate: 25,
+  },
+  {
+    value: "28-409-22",
+    label:
+      "28-409-22 - Pekerjaan Konstruksi yang Dilakukan oleh Penyedia Jasa yang Memiliki Sertifikat Badan Usaha Kualifikasi Kecil atau Sertifikat Kompetensi Kerja untuk Usaha Orang Perseorangan",
+    rate: 1.75,
+  },
+  {
+    value: "28-409-23",
+    label:
+      "28-409-23 - Pekerjaan Konstruksi yang Dilakukan oleh Penyedia Jasa yang Tidak Memiliki Sertifikat Badan Usaha Atau Sertifikat Kompetensi Kerja untuk Usaha Orang Perseorangan",
+    rate: 4,
+  },
+  {
+    value: "28-409-24",
+    label:
+      "28-409-24 - Pekerjaan Konstruksi yang Dilakukan oleh Penyedia Jasa yang Memiliki Sertifikat Selain Sertifikat Badan Usaha Kualifikasi Kecil atau Sertifikat Kompetensi Kerja untuk Usaha Orang Perseorangan",
+    rate: 2.65,
   },
 ];
 
@@ -200,7 +154,7 @@ const PPh42Form = () => {
         backgroundImage: "url('/REV1.jpg')",
       }}
     >
-      <div className="bg-white shadow-lg rounded-lg mt-4 p-6 w-full max-w-2xl">
+      <div className="bg-white/80 backdrop-blur-md shadow-2xl rounded-xl p-6 w-full max-w-2xl mt-4 mb-20 sm:mb-40 ">
         <h2 className="text-xl font-semibold mb-4">PPh 4(2)</h2>
         <div className="mb-4">
           <label className="block text-gray-700">Kode Objek Pajak</label>
